7e262763e69caffd62449b49000c0cf61ac900ec
Pranav Jetley <pranav_jetley@hotmail.com>
2014-01-31 16:21:57 +0530

    Savitzky Golay Smoothing added

--- controller/smoothing.cpp
+++ controller/smoothing.cpp
@@ -2,21 +2,23 @@
 
 #include <smoothing.h>
 #define SMA_LENGTH 5
+#define SG_LENGTH 5
 
-int history[SMA_LENGTH] = {0,};
+
+int history_SMA[SMA_LENGTH] = {0,};
 
 int simple_mov_avg(int current_value)
 {  
   int average=0,sum=0;
   for(int i=1;i<SMA_LENGTH;i++)
   { 
-    history[i-1]=history[i];
+    history_SMA[i-1]=history_SMA[i];
   }
   
-  history[SMA_LENGTH-1]=current_value;
+  history_SMA[SMA_LENGTH-1]=current_value;
   for(int j=0;j<SMA_LENGTH;j++)
   {
-    sum+=history[j];
+    sum+=history_SMA[j];
   }
   
   average=sum/SMA_LENGTH;
@@ -43,5 +45,33 @@ int exponential_mov_avg(int current_value)
   return exponential_average; 
   
 }
+
+int history_SG[SG_LENGTH] = {0,};
+const int coefficients[]={-3,12,17,12,-3};
+const int normalization_value=35;
+
+int savitzky_golay(int current_value)
+{ 
+  int sum=0;
   
+  for(int i=1;i<SG_LENGTH;i++)
+  {
+         history_SG[i-1]=history_SG[i];
+  }
+    history_SG[SG_LENGTH-1]=current_value;
+    
+  for(int i=-(SG_LENGTH-1)/2;i<(SG_LENGTH/2);i++)
+  {  
+    sum+=history_SG[i]*coefficients[i];
+  }
+  
+  history_SG[SG_LENGTH/2]=sum/normalization_value;
+  
+  return history_SG[SG_LENGTH/2];
+  
+}
+    
+  
+  
+
 
