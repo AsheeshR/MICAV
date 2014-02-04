a144935cde9d87dbabff02a8012b06dbf4007c50
Pranav Jetley <pranav_jetley@hotmail.com>
2014-02-04 15:23:05 +0530

    RDP Smoothing Algorithm added

--- controller/smoothing.cpp
+++ controller/smoothing.cpp
@@ -1,8 +1,11 @@
 //This file will contain implementations of the smoothing algorithms
 
 #include <smoothing.h>
+#include <math.h>
 #define SMA_LENGTH 5
 #define SG_LENGTH 5
+#define RDP_LENGTH 5
+#define epsilon 30
 
 
 int history_SMA[SMA_LENGTH] = {0,};
@@ -70,6 +73,61 @@ int savitzky_golay(int current_value)
   return history_SG[SG_LENGTH/2];
   
 }
+    
+int history_RDP[RDP_LENGTH] = {0,}; 
+int index_RDP[]={0,};
+
+  
+void RDP(int start_index, int end_index)
+{
+  int a = history_RDP[end_index]-history_RDP[start_index];
+  int b = -(end_index-start_index);
+  int c = -start_index*a + end_index*b;
+  
+  float max_distance = 0;
+  int max_index=0;
+  float distance = 0;
+  
+  for( int i=start_index+1;i<end_index;i++)
+  {
+    distance=(i*a-history_RDP[i]*b+c)/sqrt(a^2+b^2);
+    if (distance>max_distance)
+    { 
+      max_distance=distance;
+      max_index=i;
+    }
+  }
+  
+  if(max_distance>epsilon)
+  { 
+        RDP(start_index,max_index);
+        RDP(max_index,end_index);
+   } 
+   else 
+    
+   {
+    for( int i=start_index+1;i<end_index;i++) 
+    {
+      history_RDP[i]=(-c-a*i)/b;
+    }
+   }
+}
+   
+  int RDP_caller(int current_value)
+{ 
+  int update_value=history_RDP[0];
+  for(int i=1;i<RDP_LENGTH;i++)
+  { 
+    history_RDP[i-1]=history_RDP[i];  
+  }
+  
+  history_RDP[RDP_LENGTH-1]=current_value;
+  
+  RDP(0,RDP_LENGTH-1);
+  return update_value;
+}
+
+
     
   
   
