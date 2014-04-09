8386bff9e303b9bbd68ccebf19b7b4de1a519bef
Asheesh Ranjan <asheeshranjan1@gmail.com>
2014-04-09 08:09:38 +0530

    Pin map established

--- mav_pilot/config.h
+++ mav_pilot/config.h
@@ -1,4 +1,27 @@
 /* This defines all the configuration variables for the entire system and the pin-map */
 
 
+/* PIN Map */
+
+//#define PIN_ 0
+//#define PIN_ 1
+//#define PIN_ 2
+#define PIN_THRUST 3
+//#define PIN_ 4
+//#define PIN_PITCH 5
+//#define PIN_ROLL 6
+//#define PIN_ 7
+//#define PIN_ 8
+//#define PIN_ 9 
+//#define PIN_ 10
+//#define PIN_ 11
+//#define PIN_ 12
+//#define PIN_ 13
+#define PIN_M0 14
+#define PIN_M1 15
+#define PIN_M2 16
+#define PIN_M3 17
+//#define PIN_ 18 IMU Pins
+//#define PIN_ 19
+
 
