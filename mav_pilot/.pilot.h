40c68e98da07123a2b2e9189025f6bf537f9b17f
Asheesh <ashrj@outlook.com>
2014-06-03 11:25:36 +0530

    Fixed interrupt problems. Added critical section handling.

---
+++ mav_pilot/.pilot.h
@@ -0,0 +1,10 @@
+/* This is the header file for the main pilot program*/
+
+#include "Razor_AHRS.h"
+#include "config.h"
+#include "controller.h"
+#include "drivers.h"
+
+void start_pilot();
+
+void update_pilot();
--- mav_pilot/drivers.cpp
+++ mav_pilot/drivers.cpp
@@ -46,7 +46,7 @@ void cutoff_motors()
     esc[3].attach(PIN_MOTOR3);    esc[3].writeMicroseconds(750);
 }
 
-
+/*
 void setup_channels()
 {
     
@@ -58,17 +58,17 @@ void setup_channels()
     pinMode(PIN_AUX1, INPUT);
     pinMode(PIN_AUX2, INPUT);
 //    pinMode(PIN_AUTO, INPUT);
-
 }
+*/
 
-
+/*
 void update_channels(uint16_t channels[])
 {
 
-    channels[0] = pulseIn(PIN_THROTTLE, HIGH, 25000);
-    channels[1] = pulseIn(PIN_YAW, HIGH, 25000);
-    channels[2] = pulseIn(PIN_PITCH, HIGH, 25000);
-    channels[3] = pulseIn(PIN_ROLL, HIGH, 25000);
+    channels[0] = pulseIn(PIN_THROTTLE, HIGH, 20000);
+    channels[1] = pulseIn(PIN_YAW, HIGH, 20000);
+    channels[2] = pulseIn(PIN_PITCH, HIGH, 20000);
+    channels[3] = pulseIn(PIN_ROLL, HIGH, 20000);
   
 //    channels[4] = pulseIn(PIN_AUX1, HIGH, 25000);
 //    channels[5] = pulseIn(PIN_AUX2, HIGH, 25000);
@@ -79,7 +79,95 @@ void update_channels(uint16_t channels[])
     channels[2] = constrain(channels[2], CHANNEL3_MIN, CHANNEL3_MAX);
     channels[3] = constrain(channels[3], CHANNEL4_MIN, CHANNEL4_MAX);
 
-    
+}
+*/
+/* ISR based Control Inputs */
 
+uint8_t tmode;
+uint32_t tt1=0;
+volatile uint16_t tvalue;
+
+void updatethrottle() 
+{
+    tmode=PCintPort::pinState;  
+    if(tmode)    tt1 = micros();
+    else    tvalue = micros()-tt1;
 }
 
+uint8_t ymode;
+uint32_t yt1=0;
+volatile uint16_t yvalue;
+
+void updateyaw()
+{  
+  ymode=PCintPort::pinState;  
+  if(ymode)    yt1 = micros();
+  else yvalue = micros()-yt1;
+}
+
+uint8_t rmode;
+uint32_t rt1=0;
+volatile uint16_t rvalue;
+
+void updateroll() 
+{  
+    rmode=PCintPort::pinState;  
+    if(rmode)   rt1 = micros();
+    else    rvalue = micros()-rt1;
+}
+
+
+uint8_t pmode;
+uint32_t pt1=0;
+volatile uint16_t pvalue;
+
+void updatepitch() 
+{  
+    pmode=PCintPort::pinState;      
+    if(pmode)    pt1 = micros();
+    else pvalue = micros()-pt1;
+}
+
+
+
+void setup_channels()
+{
+    pinMode(PIN_THROTTLE, INPUT); digitalWrite(PIN_THROTTLE, HIGH);
+    PCintPort::attachInterrupt(PIN_THROTTLE, &updatethrottle, CHANGE);
+
+    pinMode(PIN_PITCH, INPUT); digitalWrite(PIN_PITCH, HIGH);
+    PCintPort::attachInterrupt(PIN_PITCH, &updatepitch, CHANGE);
+
+    pinMode(PIN_ROLL, INPUT); digitalWrite(PIN_ROLL, HIGH);
+    PCintPort::attachInterrupt(PIN_ROLL, &updateroll, CHANGE);
+
+    pinMode(PIN_YAW, INPUT); digitalWrite(PIN_YAW, HIGH);
+    PCintPort::attachInterrupt(PIN_YAW, &updateyaw, CHANGE);
+    
+//    pinMode(PIN_AUX1, INPUT);
+//    pinMode(PIN_AUX2, INPUT);
+//    pinMode(PIN_AUTO, INPUT);
+}
+
+void update_channels(uint16_t channels[])
+{
+
+    byte sregRestore = SREG;
+    cli(); // clear the global interrupt enable flag
+
+    channels[0] = tvalue;//pulseIn(PIN_THROTTLE, HIGH, 20000);
+    channels[1] = yvalue;//pulseIn(PIN_YAW, HIGH, 20000);
+    channels[2] = pvalue;//pulseIn(PIN_PITCH, HIGH, 20000);
+    channels[3] = rvalue;//pulseIn(PIN_ROLL, HIGH, 20000);
+  
+    SREG = sregRestore; // restore the status register to its previous value
+
+//    channels[4] = pulseIn(PIN_AUX1, HIGH, 25000);
+//    channels[5] = pulseIn(PIN_AUX2, HIGH, 25000);
+//    channels[6] = pulseIn(PIN_AUTO, HIGH, 25000);
+
+    channels[0] = constrain(channels[0], CHANNEL1_MIN, CHANNEL1_MAX);
+    channels[1] = constrain(channels[1], CHANNEL2_MIN, CHANNEL2_MAX);
+    channels[2] = constrain(channels[2], CHANNEL3_MIN, CHANNEL3_MAX);
+    channels[3] = constrain(channels[3], CHANNEL4_MIN, CHANNEL4_MAX);
+}
